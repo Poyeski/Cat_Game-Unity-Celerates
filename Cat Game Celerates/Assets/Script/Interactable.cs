54fb70f049af8a39a7a1432f033a49b28d4556a2
IrfanRafif08 <93048137+IrfanRafif08@users.noreply.github.com>
2024-05-28 15:44:37 +0700

    ver control 0.11
    -finished CG interaction implementation (tes pencet di diary dan drawer)
    bug 2 kali mencet masih ada
    need cleanup on anim

--- Cat Game Celerates/Assets/Script/Interactable.cs
+++ Cat Game Celerates/Assets/Script/Interactable.cs
@@ -7,13 +7,15 @@ public class Interactable : MonoBehaviour
     public string[] dialogueLines;
     public Vector3 interactionPosition;
     public string interactionAnimation;
+    public Sprite dialogueImage; // Assign an image for the dialogue
+    public int showImageAtIndex = -1; // Index at which to show the image
 
     private void OnMouseDown()
     {
         Debug.Log("Object clicked: " + gameObject.name);
         if (DialogueText.Instance != null && !DialogueText.Instance.isDialogueActive && dialogueLines.Length > 0)
         {
-            DialogueText.Instance.StartDialogue(dialogueLines);
+            DialogueText.Instance.StartDialogue(dialogueLines, dialogueImage, showImageAtIndex); // Pass the image and index to the dialogue
         }
         InteractiveMovement player = FindObjectOfType<InteractiveMovement>();
         if (player != null)
