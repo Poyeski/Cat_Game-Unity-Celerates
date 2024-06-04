594fbd0cfec2eaa738ad1a39293e428246239684
IrfanRafif08 <93048137+IrfanRafif08@users.noreply.github.com>
2024-06-05 01:34:12 +0700

    v1.9.9 (diary fix)

---
+++ Cat Game Celerates/Assets/Script/FloorColliderControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorColliderControl : MonoBehaviour
+{
+    public GameObject Floor;
+    private Animator FloorAnimator;
+
+    void Start()
+    {
+        if (Floor != null)
+        {
+            FloorAnimator = Floor.GetComponent<Animator>();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (FloorAnimator != null)
+            {
+                FloorAnimator.SetBool("OnFloor", false);
+            }
+        }
+    }
+}
